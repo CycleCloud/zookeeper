05a1a03ad54c2a0824b995d0e5935fd774156377
bwatrous <bwatrous@cyclecomputing.com>
2015-03-20 16:43:53 -0400

    Make the deploy script a bit more generic

--- deploy.sh
+++ deploy.sh
@@ -8,14 +8,27 @@ if [ "$LOCKER" == "" ]; then
 fi
 echo "Using locker: $LOCKER"
 
+EXAMPLE=zookeeper
+VERSION=0.1
+
 find . -name "*.sh" -exec chmod a+x {} \;
 find . -name "*~" -exec rm {} \;
 find . -name "*.bak" -exec rm {} \;
 
 echo "Deploying custom cookbooks..."
 pushd chef > /dev/null
-tar czf site-cookbooks.tgz site-cookbooks
-pogo put site-cookbooks.tgz ${LOCKER}/chef/zookeeper/0.1/
+if [ -d site-cookbooks ]; then
+    tar czf site-cookbooks.tgz site-cookbooks
+    pogo put site-cookbooks.tgz ${LOCKER}/chef/${EXAMPLE}/${VERSION}/
+fi
+if [ -d roles ]; then
+    tar czf roles.tgz roles
+    pogo put roles.tgz ${LOCKER}/chef/${EXAMPLE}/${VERSION}/
+fi
+if [ -d databags ]; then
+    tar czf databags.tgz databags
+    pogo put databags.tgz ${LOCKER}/chef/${EXAMPLE}/${VERSION}/
+fi
 popd > /dev/null
 
 echo "Deploying changes to cluster-init..."
